8f7e3726e2a0b8632fad148ce1a862729316ead4
Troy Fischer <troyfischer@protonmail.com>
2021-05-25 12:24:47 -0400

    change mode for bspwm, fix redshift

--- stow_all.sh
+++ stow_all.sh
@@ -5,7 +5,8 @@ stow bspwm
 stow Code
 stow polybar
 stow profile
-stow redshift
+# stow redshift - not sure why redshift isn't working with a symlinked config
+cp redshift/.config/redshift.conf ~/.config/redshift.conf
 stow scripts
 stow sxhkd
 stow user-dirs
