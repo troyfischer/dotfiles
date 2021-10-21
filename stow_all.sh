7419aa8eed4b8493010d8a8bec7b36d3718026b4
Troy Fischer <troyfischer@protonmail.com>
2021-10-20 20:54:31 -0400

    remove unused configs from stow script

--- stow_all.sh
+++ stow_all.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
 stow alacritty
-stow Code
-stow doom
-stow lf
-stow shell
-stow neovim
 stow profile
 # stow redshift - not sure why redshift isn't working with a symlinked config
 cp redshift/.config/redshift.conf ~/.config/redshift.conf
