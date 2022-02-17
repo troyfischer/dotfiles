171d056f395b1cc5df424bcc39e5add92e2a6549
troyfischer <troyfischer@protonmail.com>
2022-02-17 16:06:13 -0500

    Add emacs native comp steps

--- system_scripts/install.sh
+++ system_scripts/install.sh
@@ -74,3 +74,30 @@ then
     sudo apt update
     sudo apt install -y brave-browser
 fi
+
+# build emacs with native comp
+NAME="Emacs Native Compilation"
+read -p "Install packages $NAME? [Y/n]: " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    sudo apt install libxpm-dev libgif-dev libjpeg-dev libpng-dev libtiff-dev libx11-dev libncurses5-dev automake autoconf texinfo libgtk2.0-dev libmagickwand-dev libgnutls28-dev
+
+    sudo add-apt-repository ppa:ubuntu-toolchain-r/ppa
+
+    sudo apt update
+
+    sudo apt install gcc-10 g++-10 libgccjit0 libgccjit-10-dev libjansson4 libjansson-dev
+
+    [ ! -d ~/.local/src/emacs ] && git clone git://git.sv.gnu.org/emacs.git ~/.local/src/emacs
+
+    cd ~/.local/src/emacs || exit
+
+    git checkout emacs-28
+
+    export CC=/usr/bin/gcc-10 CXX=/usr/bin/gcc-10
+    ./autogen.sh
+    ./configure --with-cairo --with-modules --without-compress-install --with-x-toolkit=no --with-gnutls --without-gconf --without-xwidgets --without-toolkit-scroll-bars --without-xaw3d --without-gsettings --with-mailutils --with-native-compilation --with-json --with-harfbuzz --with-imagemagick --with-jpeg --with-png --with-rsvg --with-tiff --with-wide-int --with-xft --with-xml2 --with-xpm CFLAGS="-O3 -mtune=native -march=native -fomit-frame-pointer" prefix=/usr/local
+    make -j4 NATIVE_FULL_AOT=1
+    sudo make install
+fi
