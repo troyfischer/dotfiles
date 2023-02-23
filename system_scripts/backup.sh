694ab590ff91d32a7cd1f59fc2eeec79d0683123
troy <troytfischer@gmail.com>
2023-02-23 10:21:01 -0500

    update backup script

--- system_scripts/backup.sh
+++ system_scripts/backup.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 
-read -p "Is the backup device mounted to ~/tmp? [Y/n]: " -n 1 -r
+read -p "Is the backup device mounted to /Volumes/Backup? [Y/n]: " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    sudo chown troy:troy ~/tmp
-
     # backup home directory
-    rsync -aPv "$HOME/Documents" ~/tmp/ && \
-    rsync -aPv "$HOME/Calibre Library" ~/tmp/ && \
-    rsync -aPv "$HOME/dotfiles" ~/tmp/ && \
-    rsync -aPv "$HOME/dev" ~/tmp/ && \
-    rsync -aPv "$HOME/org" ~/tmp/ && \
-    rsync -aPv "$HOME/Pictures" ~/tmp/ && \
-    rsync -aPv "$HOME/Videos" ~/tmp/ && \
-    rsync -aPv "$HOME/.gnupg" ~/tmp/ && \
-    rsync -aPv "$HOME/.local/share/password-store" ~/tmp/ && \
-    rsync -aPv "$HOME/.ssh" ~/tmp/
+    rsync -aPv "$HOME/Documents" /Volumes/Backup/ && \
+    rsync -aPv "$HOME/Calibre Library" /Volumes/Backup/ && \
+    rsync -aPv "$HOME/dotfiles" /Volumes/Backup/ && \
+    rsync -aPv "$HOME/dev" /Volumes/Backup/ && \
+    rsync -aPv "$HOME/org" /Volumes/Backup/ && \
+    rsync -aPv "$HOME/blog" /Volumes/Backup/ && \
+    rsync -aPv "$HOME/Pictures" /Volumes/Backup/ && \
+    rsync -aPv "$HOME/Videos" /Volumes/Backup/ && \
+    rsync -aPv "$HOME/.gnupg" /Volumes/Backup/ && \
+    rsync -aPv "$HOME/.local/share/password-store" /Volumes/Backup/ && \
+    rsync -aPv "$HOME/.ssh" /Volumes/Backup/
 fi
