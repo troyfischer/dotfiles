828316d42a32529bcdae1b2f5a0543549107e558
Troy Fischer <troytfischer@gmail.com>
2023-10-01 18:10:15 -0500

    alacritty config

--- stow_all.sh
+++ stow_all.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-stow executables
-stow kitty
-stow ideavim
-stow nvim
-stow doom
 #stow profile
+stow alacritty
+stow doom
+stow executables
+stow ideavim
+stow kitty
+stow nvim
 stow zprofile
 stow zsh
